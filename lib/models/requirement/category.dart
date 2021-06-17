75848996e9f8a5a754adb09c90793af7fce50ce8
eunsolkang <eunsol2953@gmail.com>
2021-06-17 16:48:37 +0900

    Category Model 생성

---
+++ lib/models/requirement/category.dart
@@ -0,0 +1,26 @@
+import 'package:gateway/models/requirement/requirement.dart';
+
+class Category {
+  final String uuid;
+  final String title;
+  final String description;
+  final List<Requirement> requirements;
+
+  Category({
+    this.uuid,
+    this.title,
+    this.description,
+    this.requirements,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      uuid: json['uuid'] as String ?? "",
+      title: json['title'] as String ?? "",
+      description: json['description'] as String ?? "",
+      requirements: (json['requirements'] as List<dynamic>)
+          .map<Requirement>((json) => Requirement.fromJson(json))
+          .toList(),
+    );
+  }
+}
